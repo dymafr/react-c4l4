47390d88ced08dabf627f5f9271076bbc803a6fa
dymafr <erwan1309@hotmail.com>
2022-06-08 17:52:49 +0200

    2 files modified

--- src/Greeting.js
+++ src/Greeting.js
@@ -2,5 +2,9 @@ import React from 'react';
 import styles from './Greeting.module.scss';
 
 export default function Greeting() {
-  return <h1 className={styles.title}>Greetings</h1>;
+  return (
+    <h1 className={styles.title}>
+      Greetings <span>!!!</span>
+    </h1>
+  );
 }
